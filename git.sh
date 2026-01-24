4320b42e92baca7092f7b5438f9446032f15aceb
Mohamed_Dev <MohamedHy@git.com>
2026-01-25 00:25:18 +0200

    Temp--Message

---
+++ git.sh
@@ -0,0 +1,3 @@
+git add -A
+git commit -m "Temp--Message"
+git push
--- server/api.js
+++ server/api.js
@@ -3,22 +3,40 @@ const call = require("./functions")
 // NodeJS Initialization
 const express = require('express')
 const app = express();
+//mongodb+srv://mohamedhwtham5000_db_user:iJD7F7QO2CIQsveG@cluster0.olw39sm.mongodb.net/?appName=Cluster0
 const db = require('mongoose')
 const path = require('path')
-//--------------------------------------------
-//Variables Declaration
+
+// Variables Declaration
 let port = 8080
 let local_ip = "localhost"
 
-app.use(express.json())
+// API INIT
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../app/page.tsx'))
+db
+.connect(
+    "mongodb+srv://mohamedhwtham5000_db_user:iJD7F7QO2CIQsveG@cluster0.olw39sm.mongodb.net/?appName=Cluster0"
+)
+.then(()=>{
+    console.log('Database is Connected Successfully')
+}).catch((error)=>{
+    console.log('Database Rejected Connection Request OR An Error Has Been Occured ', error)
 })
 
 app.listen(port,local_ip,()=>{
     console.log(`Server is Running On URL :- http://${local_ip}:${port}`)
 })
+
+app.use(express.json())
+
+// The Main API
+
+app.get('/',(req,res)=>{
+    res.render('../page.ejs',{
+        "name": "mohamed"
+    })
+})
+
 
 app.get('/:num1/:num2',(req,res)=>{
     const first = Number(req.params.num1)
