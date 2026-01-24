85441ce59073d0ccc295a76162a5b459580d5763
Mohamed_Dev <MohamedHy@git.com>
2026-01-25 01:14:45 +0200

    Temp--Message

--- server/api/api.js
+++ server/api/api.js
@@ -1,17 +1,19 @@
-//Modules Initilization
-const call = require("./functions")
-// NodeJS Initialization
+//-Initilizations
+const call = require("../lib/functions")
+//--NodeJS Init
+const path = require('path')
 const express = require('express')
 const app = express();
-//mongodb+srv://mohamedhwtham5000_db_user:iJD7F7QO2CIQsveG@cluster0.olw39sm.mongodb.net/?appName=Cluster0
+//--Environment Variables Init
+require('dotenv').config(); 
+//--Database Init
 const db = require('mongoose')
-const path = require('path')
 
-// Variables Declaration
+//--Variables Declaration
 let port = 8080
 let local_ip = "localhost"
 
-// API INIT
+//--API INIT
 
 db
 .connect(
@@ -29,7 +31,7 @@ app.listen(port,local_ip,()=>{
 
 app.use(express.json())
 
-// The Main API
+//-The Main API
 
 app.get('/',(req,res)=>{
     res.render('../page.ejs',{
@@ -44,6 +46,7 @@ app.get('/:num1/:num2',(req,res)=>{
     const sum = first + second
     res.send(`${sum}`)
 })
+
 app.post('/body',(req,res)=>{
     res.send(`Hello ${req.body.name}"<br>" Your Age is ${req.body.age} "<br>" Your Hobbies are ${req.body.hobbies[0]} "<br>" Your Education is at ${JSON.stringify(req.body.education)} `)
 })
