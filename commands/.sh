57437fa5284e1c2c99b5abd00fe86ca00e380e3a
Mohamed_Dev <MohamedHy@git.com>
2026-01-25 17:05:43 +0200

    Temp--Message

---
+++ commands/.sh
@@ -0,0 +1,7 @@
+cd..
+cd server
+npm i express
+npm i nodemon
+npm i mongoose
+npm i path
+npm i dotenv
--- server/lib/functions.js
+++
@@ -1,6 +0,0 @@
-// Customized Functions Library and Module
-
-function log_res(string){
-    console.log(string)
-    res.send(`${string}`)
-}
